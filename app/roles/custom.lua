c840e269081be8f7f74995c9f56913cb16b14458
Ilya Serbis <i.serbis@it-alnc.ru>
2025-10-02 23:01:41 +0300

    refactoring: simplify code

--- app/roles/custom.lua
+++ app/roles/custom.lua
@@ -4,20 +4,15 @@ local http_client = require('http.client').new()
 local function http_weather(req)
     
     local place_name = req:query_param().place
-
     if place_name == nil or place_name == "" then
-        local resp = req:render({text = "'place' parameter is required" })
-        resp.status = 400
-        return resp
+        return { status = 400, body = "'place' parameter is required" }
     end
 
     -- TODO Does this block TX fiber?
     local response = http_client:get('https://geocoding-api.open-meteo.com/v1/search?name=' .. place_name .. '&count=1&language=en&format=json')
     local places = response:decode()['results']
     if places == nil or #places == 0 then
-        local resp = req:render({text = "'"..place_name.."' not found" })
-        resp.status = 404
-        return resp
+        return { status = 404, body = "'"..place_name.."' not found" }
     end
     local place = places[1]
     local resp = req:render({json = { latitude = place['latitude'], longitude = place['longitude'] } })
