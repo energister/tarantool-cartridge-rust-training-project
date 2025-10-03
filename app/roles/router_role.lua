58892914137e60e24938a1b47ee44bdb82d5f03e
Ilya Serbis <i.serbis@it-alnc.ru>
2025-10-03 14:51:14 +0300

    refactoring: extract router code into dedicated file

---
+++ app/roles/router_role.lua
@@ -0,0 +1,37 @@
+local cartridge = require('cartridge')
+local router = require('app.router')
+
+local function init(opts) -- luacheck: no unused args
+    -- if opts.is_master then
+    -- end
+
+    local httpd = assert(cartridge.service_get('httpd'), "Failed to get httpd service")
+
+    httpd:route({ method = 'GET', path = '/weather'}, router.http_get_weather)
+
+    return true
+end
+
+local function stop()
+    return true
+end
+
+local function validate_config(conf_new, conf_old) -- luacheck: no unused args
+    return true
+end
+
+local function apply_config(conf, opts) -- luacheck: no unused args
+    -- if opts.is_master then
+    -- end
+
+    return true
+end
+
+return {
+    role_name = 'app.roles.router',
+    init = init,
+    stop = stop,
+    validate_config = validate_config,
+    apply_config = apply_config,
+    dependencies = {'cartridge.roles.vshard-router'},
+}
---
+++ app/router.lua
@@ -0,0 +1,23 @@
+local http_client = require('http.client').new()
+
+local function http_get_weather(req)
+
+    local place_name = req:query_param().place
+    if place_name == nil or place_name == "" then
+        return { status = 400, body = "'place' parameter is required" }
+    end
+
+    -- TODO Does this block TX fiber?
+    local response = http_client:get('https://geocoding-api.open-meteo.com/v1/search?name=' .. place_name .. '&count=1&language=en&format=json')
+    local places = response:decode()['results']
+    if places == nil or #places == 0 then
+        return { status = 404, body = "'"..place_name.."' not found" }
+    end
+
+    local place = places[1]
+    return req:render({json = { latitude = place['latitude'], longitude = place['longitude'] } })
+end
+
+return {
+    http_get_weather = http_get_weather,
+}
