28bd5d8564d05941629aa7dc9d6f6b835889ec13
Ilya Serbis <i.serbis@it-alnc.ru>
2025-10-16 20:49:01 +0300

    refactoring: clean up tests

--- test/unit/data_fetcher_apply_config_test.lua
+++ test/unit/data_fetcher_apply_config_test.lua
@@ -9,7 +9,7 @@ local OPTS_ARGUMENT = { is_master = true }
 
 local settings = require('app.data_fetcher').settings.open_meteo_api
 
-g.test_default_on_start = function() -- luacheck: no unused args
+g.test_default_on_start = function()
     local custom_config = { }
 
     data_fetcher_role.apply_config(custom_config, OPTS_ARGUMENT)
@@ -17,7 +17,7 @@ g.test_default_on_start = function() -- luacheck: no unused args
     t.assert_equals(settings.request_timeout_in_seconds, settings.REQUEST_TIMEOUT_IN_SECONDS_DEFAULT)
 end
 
-g.test_apply_config = function() -- luacheck: no unused args
+g.test_apply_config = function()
     local custom_config = yaml.decode([[
     custom_config:
         open_meteo_api:
@@ -29,7 +29,7 @@ g.test_apply_config = function() -- luacheck: no unused args
     t.assert_equals(settings.request_timeout_in_seconds, 15)
 end
 
-g.test_remove_the_option_or_config = function() -- luacheck: no unused args
+g.test_remove_the_option_or_config = function()
     local custom_config = yaml.decode([[
     custom_config:
         open_meteo_api:
