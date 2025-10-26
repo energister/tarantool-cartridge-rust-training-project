a4ab99530278677d0fa6588a8fee49779b2fe6f6
Ilya Serbis <i.serbis@it-alnc.ru>
2025-10-27 00:42:42 +0300

    weather in Berlin test

--- rust/src/router.rs
+++ rust/src/router.rs
@@ -1,24 +1,25 @@
-use std::collections::HashMap;
+use serde::Serialize;
+use shors::transport::Context;
 use shors::transport::http::route::Builder;
 use shors::transport::http::{Request, Response};
-use shors::transport::Context;
-use tarantool::{lua_state};
+use std::collections::HashMap;
+use std::error::Error;
+use tarantool::lua_state;
 
 pub fn init_router() -> bool {
     let get_hello_route = Builder::new()
         .with_method("GET")
         .with_path("/hello")
-        .build(
-            |_ctx: &mut Context, _request: Request| -> Result<_, Box<dyn std::error::Error>> {
-                Ok(Response {
-                    status: 200,
-                    headers: HashMap::from([]),
-                    body: "Hello world!".as_bytes().to_vec(),
-                })
-            },
-        );
+        .build(http_hello_handler);
 
-    shors::transport::http::server::Server::new().register(Box::new(get_hello_route));
+    let get_weather_route = Builder::new()
+        .with_method("GET")
+        .with_path("/weather")
+        .build(http_weather_handler);
+
+    let server = shors::transport::http::server::Server::new();
+    server.register(Box::new(get_hello_route));
+    server.register(Box::new(get_weather_route));
 
     let lua = lua_state();
     lua.exec("require('log').error('Hello')").unwrap();
@@ -26,3 +27,55 @@ pub fn init_router() -> bool {
     true
 }
 
+fn http_hello_handler(_ctx: &mut Context, _request: Request) -> Result<Response, Box<dyn Error>> {
+    Ok(Response {
+        status: 200,
+        headers: HashMap::from([]),
+        body: "Hello world!".as_bytes().to_vec(),
+    })
+}
+
+
+#[derive(Serialize)]
+struct WeatherResponse {
+    coordinates: Coordinates,
+    temperature_celsius: f64,
+}
+#[derive(Serialize)]
+struct Coordinates {
+    latitude: f64,
+    longitude: f64,
+}
+
+fn http_weather_handler(_ctx: &mut Context, request: Request) -> Result<Response, Box<dyn Error>> {
+    let place = form_urlencoded::parse(request.query.as_bytes())
+        .find(|(key, _)| key == "place")
+        .map(|(_, value)| value.to_string());
+
+    if place.is_none() {
+        return Ok(Response {
+            status: 400,
+            headers: HashMap::from([]),
+            body: "'place' parameter is required".as_bytes().to_vec(),
+        });
+    }
+
+    let place_value = place.unwrap();
+
+    let lua = lua_state();
+    lua.exec(&format!(
+        "require('log').error('Hello from {}')",
+        place_value
+    ))
+    .unwrap();
+
+    let response = WeatherResponse {
+        coordinates: Coordinates {
+            latitude: 52.52437,
+            longitude: 13.41053,
+        },
+        temperature_celsius: 20.0,
+    };
+
+    return Ok(Response::from(response))
+}
